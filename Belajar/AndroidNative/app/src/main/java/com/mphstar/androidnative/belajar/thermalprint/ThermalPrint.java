d2de627e7a8f41ef81de8abe1959841e09318966
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-02-15 21:16:02 +0700

    Add Tugas Minggu 2

---
+++ Belajar/AndroidNative/app/src/main/java/com/mphstar/androidnative/belajar/thermalprint/ThermalPrint.java
@@ -0,0 +1,14 @@
+package com.mphstar.androidnative.belajar.thermalprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThermalPrint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thermal_print);
+    }
+}
